9d0cf2384cd1fb10272b6b12bf23281f47697e5b
bee85919 <bee85919@gmail.com>
2023-02-18 00:09:25 +0900

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,23 +2,27 @@ import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
-export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Create the Account" });
+
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
+
   if (password !== password2) {
     return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
     });
   }
+
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
     return res.status(400).render("join", {
       pageTitle,
-      errorMessage: "This username/email is already taken.",
+      errorMessage: "This email/ username is already taken.",
     });
   }
+
   try {
     await User.create({
       name,
@@ -27,28 +31,36 @@ export const postJoin = async (req, res) => {
       password,
       location,
     });
-    return res.redirect("/login");
+    return res.redirect("/login");    
   } catch (error) {
     return res.status(400).render("join", {
-      pageTitle: "Upload Video",
+      pageTitle: "Create the Account",
       errorMessage: error._message,
     });
   }
 };
-export const getLogin = (req, res) =>
+
+export const getLogin = (req, res) => {
+  if (req.session.loggedIn) {
+      return res.redirect("/");
+  }  
   res.render("login", { pageTitle: "Login" });
+};
 
 export const postLogin = async (req, res) => {
-  const { username, password } = req.body;
   const pageTitle = "Login";
+  const { username, password } = req.body;
   const user = await User.findOne({ username, socialOnly: false });
+
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
       errorMessage: "An account with this username does not exists.",
     });
   }
+
   const ok = await bcrypt.compare(password, user.password);
+
   if (!ok) {
     return res.status(400).render("login", {
       pageTitle,
@@ -66,9 +78,10 @@ export const startGithubLogin = (req, res) => {
     client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
-  };
+  };  
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
+
   return res.redirect(finalUrl);
 };
 
@@ -78,7 +91,7 @@ export const finishGithubLogin = async (req, res) => {
     client_id: process.env.GH_CLIENT,
     client_secret: process.env.GH_SECRET,
     code: req.query.code,
-  };
+  };  
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   const tokenRequest = await (
@@ -89,6 +102,7 @@ export const finishGithubLogin = async (req, res) => {
       },
     })
   ).json();
+
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
     const apiUrl = "https://api.github.com";
@@ -109,11 +123,13 @@ export const finishGithubLogin = async (req, res) => {
     const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!emailObj) {
-      // set notification
+    
+    if (!emailObj) {      
+      req.flash("error", "Can't finish Github Login.");
       return res.redirect("/login");
     }
-    let user = await User.findOne({ email: emailObj.email });
+
+    let user = await User.exists({ email: emailObj.email });
     if (!user) {
       user = await User.create({
         avatarUrl: userData.avatar_url,
@@ -133,12 +149,6 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {
-  req.session.destroy();
-  req.flash("info", "Bye Bye");
-  return res.redirect("/");
-};
-
 export const getEdit = (req, res) => {
   return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
@@ -146,11 +156,30 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id, avatarUrl },
+      user: { _id, avatarUrl, email: sessionEmail, username: sessionUsername },
     },
     body: { name, email, username, location },
     file,
   } = req;
+
+  let searchParam = [];
+  if (sessionEmail !== email) {
+      searchParam.push({ email });
+  }
+  if (sessionUsername !== username) {
+      searchParam.push({ username });
+  }
+  if (searchParam.length > 0) {
+      const foundUser = await User.findOne({ $or: searchParam });
+      //$or은 여러 개의 조건 중에 적어도 하나를 만족하는 다큐먼트를 찾습니다.
+      if (foundUser && foundUser._id.toString() !== _id) {
+          return res.status(400).render("edit-profile", {
+              pageTitle: "Edit Profile",
+              errorMessage: "This email/ username is already taken.",
+          });
+      }
+  }
+  
   const isHeroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
@@ -161,10 +190,18 @@ export const postEdit = async (req, res) => {
       username,
       location,
     },
-    { new: true }
+    { 
+      new: true 
+    }
   );
   req.session.user = updatedUser;
   return res.redirect("/users/edit");
+};
+
+export const logout = (req, res) => {
+  req.session.destroy();
+  req.flash("info", "Bye Bye");
+  return res.redirect("/");
 };
 
 export const getChangePassword = (req, res) => {
@@ -174,6 +211,7 @@ export const getChangePassword = (req, res) => {
   }
   return res.render("users/change-password", { pageTitle: "Change Password" });
 };
+
 export const postChangePassword = async (req, res) => {
   const {
     session: {
@@ -183,6 +221,7 @@ export const postChangePassword = async (req, res) => {
   } = req;
   const user = await User.findById(_id);
   const ok = await bcrypt.compare(oldPassword, user.password);
+
   if (!ok) {
     return res.status(400).render("users/change-password", {
       pageTitle: "Change Password",
@@ -214,7 +253,7 @@ export const see = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
   return res.render("users/profile", {
-    pageTitle: user.name,
+    pageTitle: `${user.name}'s profile`,
     user,
   });
 };
